c84a323db0e38d834834e76bb0d50aabc4825174
j9recurses <janine.heiser@gmail.com>
2017-05-24 16:59:39 -0700

    adds stuff to handle cors

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ let bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 let apiRouter = express.Router()
 app.use('/api/v1', apiRouter)
 
